ec44478be817c7848e5dcb0a4b2269627c1dd7b3
jcaesar <gitter@liftm.de>
2018-05-18 13:21:00 +0900

    perm_itertools.py: Benchmark appending vs generator materialization vs no materialization

--- perm_itertools.py
+++ perm_itertools.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 #-*-coding:utf8-*-
 
 import itertools
@@ -6,11 +7,35 @@ import numpy as np
 
 a = np.array(['a','b','c','d','e','f','g','h','i','j'])
 
-pat_list = []
-start = datetime.datetime.now()
-for i in range(1,len(a)+1):
-    print (i)
-    pat_list.append(list(itertools.permutations(a,i)))
 
-end = datetime.datetime.now()
-print ((end-start).total_seconds())
+def nplist():
+    start = datetime.datetime.now()
+    pat_list = []
+    for i in range(1,len(a)+1):
+        list(itertools.permutations(a,i))
+
+    end = datetime.datetime.now()
+    print ("nplist: ", (end-start).total_seconds())
+
+def allsubperms(a):
+    for i in range(1,len(a)+1):
+        for e in itertools.permutations(a,i):
+            yield e
+
+def npgen():
+    start = datetime.datetime.now()
+    pat_list = list(allsubperms(a))
+    end = datetime.datetime.now()
+    print ("npgen: ", (end-start).total_seconds())
+
+def npnomat():
+    start = datetime.datetime.now()
+    for e in allsubperms(a):
+        pass
+    end = datetime.datetime.now()
+    print ("npnomat: ", (end-start).total_seconds())
+
+for i in range(1,7):
+    nplist()
+    npgen()
+    npnomat()
